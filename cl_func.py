76978296e9c355a0fc34d217604f820b02a89228
Kyle Stockton <kylestockton@mail.com>
2017-04-04 03:11:12 -0700

    working prototype

--- cl_func.py
+++ cl_func.py
@@ -1,22 +1,22 @@
 task_read = []
-time_read = []
-task_in = ['',['eat'],['drink',['liquor',['scotch'],['tequila']],['beer']],['be merry']]
-time_in = [0,[1],[3,[2,[1],[1]],[1]],[3]]
+code_read = []
+task_in = ['',['planning'],['implementation',['R & D',['research'],['development']],['documentation']],['cleanup']]
+code_in = ["00:00:00:00",["00:00:01:00"],["00:00:03:00",["00:00:02:00",["00:00:01:00"],["00:00:01:00"]],["00:00:01:00"]],["00:00:02:00"]]
 
 def save_tasklist():
     update_arrays()
     file = open(path,'w')
-    file.write(str(task_write) + "\n" + str(time_write))
+    file.write(str(task_write) + "\n" + str(code_write))
     file.close()
     print(task_write)
-    print(time_write)
+    print(code_write)
 
 def load_tasklist(path = 'default.txt'):
     file = open(path,'r')
     task_read = eval(file.readline())
     print(task_read)
-    time_read = eval(file.readline())
-    print(time_read)
+    code_read = eval(file.readline())
+    print(code_read)
     file.close()
-    return task_read, time_read
+    return task_read, code_read
 
--- main.py
+++ main.py
@@ -1,114 +1,201 @@
 import time
 from tkinter import *
 from tkinter import ttk
-from cl_func import *
+from pygame import mixer
 
-t0 = time.time()
-root = Tk()
-task_read, time_read = load_tasklist()
-flat_dict = {'':0}
-task_write = ['']
-time_write = [0]
+default_timecode = "00:00:00:05"
+task_read = ['',['New task 1']]
+code_read = ["00:00:00:00",[default_timecode]]
 lvl_arr = []
 parent_arr = ['']
-newitem_inc = 0
+del_arr = []
+newitem_inc = 1
+mixer.init()
+mixer.music.load('Alarm01.wav')
+root = Tk()
 
-
+# reads existing arrays and generates task tree
 def populate_tree():
     global t0
     task_temp = task_read
-    time_temp = time_read
-    print(lvl_arr)
+    code_temp = code_read
+
     for i in lvl_arr:
         task_temp = task_temp[i]
-        time_temp = time_temp[i]
+        code_temp = code_temp[i]
     for t in range(1,len(task_temp)):
-        print("Adding " + task_temp[t][0])
         id = tree.insert(parent_arr[-1], 'end', \
-            text=task_temp[t][0])
-        flat_dict[id] = t0 + (time_temp[t][0] * 3600)
+            text=task_temp[t][0], values=(code_temp[t][0]))
         if len(task_temp[t]) > 0:
             parent_arr.append(id)
             lvl_arr.append(t)
             populate_tree()
             lvl_arr.pop()
             parent_arr.pop()
-    print("Done")
 
-def update_arrays():
-    task_temp = task_write
-    time_temp = time_write
-    for i in lvl_arr:
-        task_temp = task_temp[i]
-        time_temp = time_temp[i]
-    for c in tree.get_children(parent_arr[-1]):
-        task_temp.append([tree.item(c,'text')])
-        time_temp.append([int(tree.item(c,'values'[0])[0])])
-        if len(tree.get_children(c)) > 0:
-            parent_arr.append(c)
-            lvl_arr.append(tree.index(c)+1)
-            update_arrays()
-            lvl_arr.pop()
-            parent_arr.pop()
-    
+# creates new leaf in task tree    
 def append_tree():
     global newitem_inc
-    id = tree.insert(tree.selection()[0], 'end', text="new item " + str(newitem_inc), values=(0))
     newitem_inc += 1
+    id = tree.insert(tree.selection()[0], 'end', text="New task " + str(newitem_inc), values=(default_timecode))
+    update_parent(id)
     tree.see(id)
 
-def countdown():
-    for f in flat_dict:
-        t = int(flat_dict[f] - time.time())
-        s = t % 60
-        t = (t-s) // 60
-        m = t % 60
-        t = (t-m) // 60
-        h = t % 24
-        d = (t-h) // 24
-        tree.set(f, 'Time', "{:02d}:{:02d}:{:02d}:{:02d}".format(d,h,m,s))
-
-def countdown_loop():
-    countdown()
-    root.after(500, countdown_loop)
-
-
-def task_details():
-
+# clears pane display and calls methods to repopulate
+def display_tasks(event):
+    for k in del_arr:
+        k.destroy()
+    del_arr.clear()
+    # get ancestry of selected tasks
+    display_hierarchy(tree.selection())
+    # get selected task and siblings
+    display_children(tree.parent(tree.selection()), siblings)
+    # get children of selected task
+    display_children(tree.selection(), children)
+    print(tree.parent(tree.selection()))
     return 0
 
+# recursively populates hierarchy frame
+def display_hierarchy(task):
+    if tree.parent(task) != '':
+        # determines row number
+        i = 0
+        s = task
+        while s != '':
+            i += 1
+            s = tree.parent(s) 
+        # passes arguments to task_details for current level 
+        task_details(tree.parent(task), hierarchy, i)
+        # calls self for next level up
+        display_hierarchy(tree.parent(task))
+    return 0
+
+# populates children frame and sibling frame (using parent of selected task)
+def display_children(task, frame):
+    i = 0
+    for t in tree.get_children(task):
+        task_details(t, frame, i)
+        i += 1
+    return 0
+
+# alternate method to populate siblings frame
+def display_siblings(task):
+    i = 1
+    for t in tree.get_children(tree.parent(task)):
+        if t == task:
+            task_details(t, siblings, 0)
+        else:
+            task_details(t, siblings, i)
+        i += 1
+    return 0
+
+# creates a single task entry in paned view
+def task_details(task, frame, row_num):
+    id = ttk.Label(frame, width=100, \
+        text=tree.item(task,'text'), anchor=NE, relief='raised')
+    task_lbl = ttk.Label(id, text="Task:").grid(column=0, row=0)
+    task_e = ttk.Entry(id, textvariable = tree.item(task,'text'))
+    task_e.grid(column=1,row=0)
+    task_e.bind('<Return>',lambda e: print(tree.item(task, text=task_e.get())))
+    code_lbl = ttk.Label(id, text="Time:").grid(column=2, row=0)
+    code_e = ttk.Entry(id, textvariable = tree.set(task,'Time'))
+    code_e.grid(column=3,row=0)
+    code_e.bind('<Return>',lambda e: print(update_entry(task,code_e.get())))
+    id.grid(column=0, row=row_num)
+    del_arr.append(id)
+    return 0
+
+# updates timecode for corresponding task
+def update_entry(task, time):
+    tree.set(task,'Time',time)
+    update_parent(tree.parent(task))
+    return 0
+
+# updates timecode for corresponding task according to changes to children
+def update_parent(task):
+    if task != '':
+        i = 0
+        for t in tree.get_children(task):
+            i += convert_to_seconds(tree.set(t,'Time'))
+        p = max(i, convert_to_seconds(tree.set(task, 'Time')))
+        tree.set(task, 'Time', convert_to_timecode(p))
+        update_parent(tree.parent(task))
+    return 0
+
+# converts timecode to number of seconds
+def convert_to_seconds(t):
+    arr = list(map(int, t.split(':')))
+    return arr[0] * 86400 + arr[1] * 3600 + arr[2] * 60 + arr[3]
+
+# converts number of seconds to timecode
+def convert_to_timecode(t):
+    s = t % 60
+    t = (t-s) // 60
+    m = t % 60
+    t = (t-m) // 60
+    h = t % 24
+    d = (t-h) // 24
+    return "{:02d}:{:02d}:{:02d}:{:02d}".format(d,h,m,s)
+
+# calls countdown method as part of tk loop
+def countdown_loop():
+    countdown('')
+    root.after(1000, countdown_loop)
+
+# calculates incremental change in time remaining and updates relevant fields
+def countdown(task):
+    for t in tree.get_children(task):
+        if tree.set(t, 'Time') != "00:00:00:00":
+            i = convert_to_seconds(tree.set(t,'Time'))
+            i -= 1
+            tree.set(t, 'Time', convert_to_timecode(i))
+            if i == 0:
+                mixer.music.play()
+            if len(tree.get_children(t)) > 0:
+                countdown(t)
+            break
+
 content = ttk.Frame(root, padding=(3,3,12,12))
+
+# create and populate tree view
 tree = ttk.Treeview(content, columns=('Time'))
-tree.set('', 'Time', t0 + (7 * 3600)) 
 populate_tree()
-namelbl = ttk.Label(content, text="Name")
-name = ttk.Entry(content)
-print(flat_dict)
-countdown_loop()
 
-onevar = BooleanVar()
-twovar = BooleanVar()
-threevar = BooleanVar()
-onevar.set(True)
-twovar.set(False)
-threevar.set(True)
+# create paned view
+details = ttk.PanedWindow(content, orient=VERTICAL)
+hier_lbl = ttk.LabelFrame(details, text="Hierarchy", width=100, height=50)
+hierarchy = Canvas(hier_lbl, height=150)
+hier_scroll = ttk.Scrollbar(hier_lbl, orient=VERTICAL, command=hierarchy.yview)
+hierarchy.configure(yscrollcommand=hier_scroll.set)
+sibs_lbl = ttk.LabelFrame(details, text="Siblings", width=100, height=50)
+siblings = Canvas(sibs_lbl, height=150)
+sibs_scroll = ttk.Scrollbar(sibs_lbl, orient=VERTICAL, command=hierarchy.yview)
+chil_lbl = ttk.LabelFrame(details, text="Children", width=100, height=50)
+children = Canvas(chil_lbl, height=150)
+chil_scroll = ttk.Scrollbar(chil_lbl, orient=VERTICAL, command=hierarchy.yview)
+details.add(hier_lbl)
+details.add(sibs_lbl)
+details.add(chil_lbl)
 
-one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
-two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
-three = ttk.Checkbutton(content, text="Three", variable=threevar, \
-    onvalue=True)
+# create buttons
 new_button = ttk.Button(content, text="Add", command=append_tree)
-save_button = ttk.Button(content, text="Save", command=save_tasklist)
+start_button = ttk.Button(content, text="Start", command=countdown_loop)
 
+# grid configuration of constant elements
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 tree.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
-namelbl.grid(column=3, row=0, columnspan=2, sticky=(N, W), padx=5)
-name.grid(column=3, row=1, columnspan=2, sticky=(N, E, W), pady=5, padx=5)
-one.grid(column=0, row=3)
-two.grid(column=1, row=3)
-three.grid(column=2, row=3)
+details.grid(column=3, row=0, columnspan=2, rowspan=2, \
+    sticky=(N, W), padx =5, pady=5)
+
+hierarchy.grid(column=0, row=0, sticky=(N, S, E, W))
+hier_scroll.grid(column=1, row=0, sticky=(N, S, E))
+siblings.grid(column=0, row=1, sticky=(N, S, E, W))
+sibs_scroll.grid(column=1, row=1, sticky=(N, S, E))
+children.grid(column=0, row=2, sticky=(N, S, E, W))
+chil_scroll.grid(column=1, row=2, sticky=(N, S, E))
+
 new_button.grid(column=3, row=3)
-save_button.grid(column=4, row=3)
+start_button.grid(column=4, row=3)
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
@@ -119,7 +206,50 @@ content.columnconfigure(3, weight=1)
 content.columnconfigure(4, weight=1)
 content.rowconfigure(1, weight=1)
 
-tree.bind('<<TreeviewSelect>>', \
-    lambda e: print(tree.item(tree.selection())))
+tree.bind('<<TreeviewSelect>>', display_tasks)
 
 root.mainloop()
+
+
+
+def update_arrays():
+    task_temp = task_write
+    code_temp = code_write
+    for i in lvl_arr:
+        task_temp = task_temp[i]
+        code_temp = code_temp[i]
+    for c in tree.get_children(parent_arr[-1]):
+        task_temp.append([tree.item(c,'text')])
+        code_temp.append([int(tree.item(c,'values'[0])[0])])
+        if len(tree.get_children(c)) > 0:
+            parent_arr.append(c)
+            lvl_arr.append(tree.index(c)+1)
+            update_arrays()
+            lvl_arr.pop()
+            parent_arr.pop()
+
+def task_details_spinbox(task,row_num):
+    id = ttk.Label(hierarchy, width=100, \
+        text=tree.item(task,'text'), anchor=NE, relief='raised')
+    src_lbl = ttk.Label(id, text=task)
+    entry = ttk.Entry(id, textvariable = tree.item(task,'text'))
+    entry.grid(column=0,row=0)
+    entry.bind('<KeyPress>',lambda e: print(tree.item(task, text=entry.get())))
+    d_lbl = ttk.Label(id, text='Days').grid(column=0, row=1)
+    d_box = Spinbox(id, from_=0, to=59, wrap=True)
+    d_box.grid(column=0, row=2)
+    h_lbl = ttk.Label(id, text='Hours').grid(column=1, row=1)
+    h_box = Spinbox(id, from_=0, to=59, wrap=True)
+    h_box.grid(column=1, row=2)
+    m_lbl = ttk.Label(id, text='Minutes').grid(column=2, row=1)
+    m_box = Spinbox(id, from_=0, to=59, wrap=True)
+    m_box.grid(column=2, row=2)
+    s_lbl = ttk.Label(id, text='Seconds').grid(column=3, row=1)
+    s_box = Spinbox(id, from_=0, to=59, wrap=True)
+    s_box.grid(column=3, row=2)
+    id.bind('<Enter>',assign_vars(task,d_box,h_box,m_box,s_box))
+    id.bind('<Leave>',clear_vars(task,d_box,h_box,m_box,s_box))
+    id.grid(column=0, row=row_num)
+    del_arr.append(id)
+    return 0
+
